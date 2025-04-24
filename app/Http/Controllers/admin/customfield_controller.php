ff68fdec80a9a3605f1e71800115d8ec136e99ad
Mounepmohammad <monepmohammad8@gmail.com>
2025-04-24 18:07:45 +0300

    new version

--- app/Http/Controllers/admin/customfield_controller.php
+++ app/Http/Controllers/admin/customfield_controller.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\admin;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
-
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\fieldRequest;
 use Validator;
